f2f3ea7fbc0e5205d1772e3d22144b8ab4d73bec
Evgeniya Shiyanova <evgenshiy@gmail.com>
2024-09-04 23:41:24 +0500

    Added getMessage() method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.println("Hellow Kitty!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello Kitty!";
     }
 }
